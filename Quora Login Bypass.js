53a3ad4cfea3bd1fee472cccd4378b927bfbf8dd
Hemang Sarkar <hemangsarkar@gmail.com>
2017-01-30 23:20:33 +0530

    Made some changes to detect login

--- Quora Login Bypass.js
+++ Quora Login Bypass.js
@@ -5,20 +5,27 @@ var currentlocation = window.location.href;
 var str = "?share=1";
 var flag = 1;
 
-//if already logged in, do nothing
-var generatedSource = new XMLSerializer().serializeToString(document);
-var str2 = '<span class="expanded">Notifications';
-if(generatedSource.indexOf(str2)!=-1){
-flag = 0;
+
+if(currentlocation.indexOf("www.quora.com")!=-1){
+	//if already logged in, do nothing
+	var generatedSource = new XMLSerializer().serializeToString(document);
+	var str2 = '<span class="expanded">Notifications';
+
+
+
+	if(generatedSource.indexOf(str2)!=-1){
+	flag = 0;
+	}
+
+	//if not logged in then add "?share=1" at the end of the current address
+	else if(flag && currentlocation.indexOf(str)==-1 && currentlocation!="https://www.quora.com/"){
+	window.location.href = currentlocation + str;
+	}
+
+	//If you open "https://www.quora.com/", then it will redirect to this random page. Now you 
+	//can search and browse without logging in. 
+	else if(flag && currentlocation=="https://www.quora.com/"){
+	window.location.href = "https://www.quora.com/search?q=quora+is+awesome&type=question%3Fshare%3D1%3Fshare%3D1?share=1"
+	}	
 }
 
-//if not logged in then add "?share=1" at the end of the current address
-else if(flag && currentlocation.indexOf(str)==-1 && currentlocation!="https://www.quora.com/"){
-window.location.href = currentlocation + str;
-}
-
-//If you open "https://www.quora.com/", then it will redirect to this random page. Now you 
-//can search and browse without logging in. 
-else if(flag && currentlocation=="https://www.quora.com/"){
-window.location.href = "https://www.quora.com/search?q=quora+is+awesome&type=question%3Fshare%3D1%3Fshare%3D1?share=1"
-}
