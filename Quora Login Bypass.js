72a36e210e68c21a27ae90621e83c9ea691e4456
Hemang Sarkar <hemangsarkar@gmail.com>
2017-01-30 22:19:53 +0530

    Made some changes

--- Quora Login Bypass.js
+++ Quora Login Bypass.js
@@ -1,5 +1,15 @@
 var currentlocation = window.location.href;
 var str = "?share=1";
+
+// Quora have themselves published this to share without restriction
+// https://blog.quora.com/Making-Sharing-Better
+
 if(currentlocation.indexOf(str)==-1 && currentlocation!="https://www.quora.com/"){
 window.location.href = currentlocation + str;
+}
+
+//If you open "https://www.quora.com/", then it will redirect to this random page. Now you 
+//can search and browse without loggin in. 
+else if(currentlocation=="https://www.quora.com/"){
+window.location.href = "https://www.quora.com/search?q=quora+is+awesome&type=question%3Fshare%3D1%3Fshare%3D1?share=1"
 }
